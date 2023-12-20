54e1c0e51eece9fceedb0abf446db024a5dde05c
hakan <tasbeyhakan@gmail.com>
2023-12-20 20:05:07 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+import Projects from "./pages/projects/Projects";
+import { Routes, Route } from "react-router-dom";
+import "./scss/main.scss";
+import NotFound from "./components/Error/NotFound";
+import { useLocation } from "react-router-dom/dist";
+
 function App() {
+
+  const location = useLocation();
+
+  const getPagesStyles = () => {
+    if (location.pathname === "/") {
+      return "home-bgImg-container";
+    } else if (location.pathname === "/about") {
+      return "about-bgImg-container";
+    } else if (location.pathname === "/projects") {
+      return "project-bgImg-container";
+    } else if (location.pathname === "/contact") {
+      return "contact-bgImg-container";
+    } 
+  };
+
   return (
-    <div>
-      <h1>React App</h1>
+    <div className={getPagesStyles()}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import Project1 from "../../assets/img/project-1.jpg"
+import Project2 from "../../assets/img/project-2.jpg"
+import Project3 from "../../assets/img/project-3.jpg"
+import Project4 from "../../assets/img/project-4.jpg"
+import Project5 from "../../assets/img/project-5.jpg"
+import Project6 from "../../assets/img/project-6.jpg"
+
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+  <div >
+    <div className="projects__bio-image">
+      <h1>My Projects</h1>
+    </div>
+  </div>
+  {/* PROJECT MAIN */}
+  <main className="projects">
+    <div className="projects__items">
+      <div className="projects__item">
+        <img src={Project1} alt="My Project" />
+      </div>
+      <div className="projects__item">
+        <img src={Project2} alt="My Project" />
+      </div>
+      <div className="projects__item">
+        <img src={Project3} alt="My Project" />
+      </div>
+      <div className="projects__item">
+        <img src={Project4} alt="My Project" />
+      </div>
+      <div className="projects__item">
+        <img src={Project5} alt="My Project" />
+      </div>
+      <div className="projects__item">
+        <img src={Project6} alt="My Project" />
+      </div>
+    </div>
+  </main>
+ 
+</>
+
   )
 }
 
